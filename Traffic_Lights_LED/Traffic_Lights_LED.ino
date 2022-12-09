eb4af214bccbd3b11d90660a7527fec7d87e0ec4
Saiprasad Venkatesh Anvekar <109942017+Mr-electrologic@users.noreply.github.com>
2022-12-09 16:05:43 +0530

    Update Traffic_Lights_LED.ino

--- Traffic_Lights_LED/Traffic_Lights_LED.ino
+++ Traffic_Lights_LED/Traffic_Lights_LED.ino
@@ -9,16 +9,13 @@ void setup() {
 
 }
 void loop() {
-  digitalWrite(GREEN, HIGH);
-  delay(5000);
-  digitalWrite(YELLOW, HIGH);
-  delay(1000);
-  digitalWrite(GREEN, LOW);
-  delay(1000);
   digitalWrite(RED, HIGH);
   delay(5000);
-  digitalWrite(YELLOW, LOW);
-  delay(1000);
   digitalWrite(RED, LOW);
-  delay(1000);
+  digitalWrite(YELLOW, HIGH);
+  delay(500);
+  digitalWrite(YELLOW, LOW);
+  digitalWrite(GREEN, HIGH);
+  delay(3000);
+  digitalWrite(GREEN, LOW);
 }
